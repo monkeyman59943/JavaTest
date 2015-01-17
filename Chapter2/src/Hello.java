69c12d235b9d381dfbe8921c8aeed47fbb26a761
Jacob Johnson <jacob@debian>
2015-01-17 05:59:07 -0600

    Creating an issue on line 5 of Chapter2/src/Hello.java

--- Chapter2/src/Hello.java
+++ Chapter2/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello Worl");
         System.out.println("Let's hope this works");
     }
 }
