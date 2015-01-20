8ae3bc96156dc5d31ab60d5bc2626e4042813292
monkeyman59943 <monkeyman5943@yahoo.com>
2015-01-20 12:31:14 -0600

    Added the bare Dogs class

---
+++ Chapter2/src/Dogs.java
@@ -0,0 +1,11 @@
+package Chapter2.src;
+
+/**
+ * Created by Jacob on 1/20/2015.
+ */
+public class Dogs {
+    Dogs()
+    {
+
+    }
+}
--- Chapter2/src/Hello.java
+++ Chapter2/src/Hello.java
@@ -1,11 +1,16 @@
+package Chapter2.src;
+
+
 import java.util.*;
 
 public class Hello {
     public static void main(String[] args)
     {
-
+//        Dogs dolla = new Dogs();
 		System.out.println("Hello Worlds");
 
         System.out.println("Let's hope this works");
     }
 }
+
+
