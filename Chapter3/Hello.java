01c0ccc3ebf0afb70237eac8015b60368b016376
jacob <monkeyman5943@yahoo.com>
2015-01-26 02:32:12 -0600

    I don't know what I did this time.  I made the Chapter3 package adn then made a test class in it called hello

---
+++ Chapter3/Hello.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+/**
+ * Created by jacob on 1/24/15.
+ */
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.printf("This is a test");
+    }
+}
