6d4196ccdc395b7aeab06a42782fc8ec77178ab2
jacob <monkeyman5943@yahoo.com>
2015-01-28 05:59:56 -0600

    Fixed the CheckDate class and finished the logic in HeartRateRunner to make sure the correct date is entered.

--- Chapter3/CheckDate.java
+++ Chapter3/CheckDate.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.time.LocalDateTime;
 
 public class CheckDate
 {
 	static int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
 
+    static LocalDateTime currentDateTime = LocalDateTime.now();
 	public CheckDate()
 	{
 
@@ -11,7 +13,11 @@ public class CheckDate
 
 	public static boolean checkDay(int d, int m)
 	{
-		if(d > 0 && d < days[m] )
+		if(m<=0)
+		{
+			return false;
+		}
+		if(d > 0 && d <= days[m-1])
 		{
 			return true;
 		}
@@ -20,4 +26,41 @@ public class CheckDate
 			return false;
 		}
 	}
+	public static boolean checkYear(int y)
+    {
+        if(currentDateTime.getYear()-220<0)
+        {
+            System.out.println("The year you have entered is too low.");
+            return false;
+        }
+        if(y > currentDateTime.getYear())
+        {
+            System.out.println("The year you have entered is beyond the current year.");
+            return false;
+        }
+        else return true;
+    }
+    public static boolean checkDate(int d, int m, int y)
+    {
+    	if( y == currentDateTime.getYear())
+    	{
+    		if(m > currentDateTime.getMonthValue())
+    		{
+    			return false;
+    		}
+    		else if(m == currentDateTime.getMonthValue())
+    		{
+    			if(d>currentDateTime.getDayOfMonth())
+    			{
+    				return false;
+    			}
+
+    		}
+    		else
+    		{
+    			return true;
+    		}
+    	}
+    	return true;
+    }
 }
--- Chapter3/HeartRateRunner.java
+++ Chapter3/HeartRateRunner.java
@@ -5,22 +5,60 @@ import java.util.Scanner;
  */
 public class HeartRateRunner {
     public static void main(String[] args) {
+    	int counter = 0;
     	Scanner scan = new Scanner(System.in);
     	int day=0,year=0,month=0;//they are null for some logic to confirm they are entering correct days, months, and years
     	String last="",first="";
+    	//This whole loop is needed for the user to input their own values.
+    	//start of the do while loop that makes sure a valid date is entered
+       	do
+       	{
+       		//This is used to print the message asking for valid answers.  
+       		//I couldn't think of another way to make sure this only goes on the second run
+       		if(counter>0)
+       		{
+       			for(int i =0; i<5; i++)
+       			{
+       				System.out.println();
+       			}
+       			System.out.println("The date you entered is beyond the current date. \nPlease enter a date that has happened");
+       		}
+        System.out.println("What month were you born?");
+        month = scan.nextInt();
+        while(month>12||month<1)
+        {
+            System.out.println("That is not a valid month please type a month that is between 1 and 12");
+            month = scan.nextInt();
+        }
     	System.out.println("What day were you born?");
     	day = scan.nextInt();
-    	System.out.println("What month were you born?");
-    	month = scan.nextInt();
+        while(!CheckDate.checkDay(day,month))
+        {
+            System.out.println("That is not a valid day.  Please enter a day that is valid.");
+            day = scan.nextInt();
+        }
+
     	System.out.println("what year were you born?");
     	year = scan.nextInt();
+    	while(!CheckDate.checkYear(year))
+    	{
+    		System.out.println("please type in a valid year.");
+    		year = scan.nextInt();
+    	}
+    	counter++;
+    	}while(!CheckDate.checkDate(day,month,year));
+    	//end of the do while loop that makes a check for the valid date and repeats if it is beyond the current date
+
+
+
+
     	System.out.println("What is your first name?");
     	first = scan.next();scan.nextLine();//The scan.nextLine() is used to clear the input stream
     	System.out.println("What is your last name?");
     	last = scan.next();scan.nextLine();//The scan.nextLine() is used to clear the input stream
 
 
-
+    	//This is where the person is created
         HeartRates Person = new HeartRates(day, month, year, first, last);
         //This is for clarity on where the output starts
         for(int i = 0;i<10; i++)
