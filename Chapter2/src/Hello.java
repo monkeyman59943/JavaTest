742951c38ceca4c02eee36e4645a2b9f3a34c410
Jacob <monkeyman5943@yahoo.com>
2015-02-16 15:37:12 -0600

    This is project 5 There are more things to be added, this is just the roughest version I could make.

--- Chapter2/src/Hello.java
+++ Chapter2/src/Hello.java
@@ -1,4 +1,4 @@
-package Chapter2.src;
+//package Chapter2.src;
 
 
 import java.util.*;
@@ -6,14 +6,14 @@ import java.util.*;
 public class Hello {
     public static void main(String[] args)
     {
-    	Dogs dolla = new Dogs();
-    	dolla.printThis();
-		System.out.println("Hello Worlds");
-
-
-
-		System.out.println("This may not work");
-        System.out.println("Let's hope this works");
+//    	Dogs dolla = new Dogs();
+//    	dolla.printThis();
+//		System.out.println("Hello Worlds");
+//
+//
+//
+//		System.out.println("This may not work");
+//        System.out.println("Let's hope this works");
     }
 }
 
