1bda1cc2e02d2059698e62a8a8bf9bfb59238cd8
Jacob <monkeyman5943@yahoo.com>
2015-03-03 22:58:41 -0600

    Created Project7 and created Employee.java and Test.java

---
+++ Project7/Employee.java
@@ -0,0 +1,21 @@
+package Project7;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Jacob on 3/3/2015.
+ */
+public class Employee {
+    private double EmployeeID;
+    private BigDecimal FedTax;
+    private BigDecimal StateTax;
+    private BigDecimal TotalTax;
+    private BigDecimal NetPay;
+
+    public Employee()
+    {
+
+    }
+
+
+}
---
+++ Project7/Test.java
@@ -0,0 +1,10 @@
+package Project7;
+
+/**
+ * Created by Jacob on 3/3/2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
